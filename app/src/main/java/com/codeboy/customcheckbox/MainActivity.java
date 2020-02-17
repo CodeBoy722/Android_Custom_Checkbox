5e37cb33edd4ec405fab17953c7ed8878973e90a
moforemmanuel <moforemmanuel722@gmail.com>
2020-02-17 16:37:23 +0100

    finish

--- app/src/main/java/com/codeboy/customcheckbox/MainActivity.java
+++ app/src/main/java/com/codeboy/customcheckbox/MainActivity.java
@@ -1,40 +1,51 @@
 package com.codeboy.customcheckbox;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
-
+    CheckBox normal;
+    CheckBox square;
+    CheckBox round;
+    CheckBox star;
+    CheckBox tick;
+    CheckBox cancel;
+    ConstraintLayout parent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        parent = findViewById(R.id.parent);
         CheckBox normal = findViewById(R.id.normal);
+        CheckBox square = findViewById(R.id.square);
+        CheckBox round = findViewById(R.id.round);
+        CheckBox star = findViewById(R.id.star);
+        CheckBox tick = findViewById(R.id.tick);
+        CheckBox cancel = findViewById(R.id.cancel);
 
-        /*fab.setOnClickListener(view -> {
-            if(mBound){
-                EqualizerFragment equalizer = new EqualizerFragment();
-                equalizer.show(getSupportFragmentManager(),"equalizer");
-            }else{
-                Snackbar.make(view, MainActivity.this.getString(R.string.equalizer_show), Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }*/
-
-
-
+        normal.setOnCheckedChangeListener(this);
+        square.setOnCheckedChangeListener(this);
+        round.setOnCheckedChangeListener(this);
+        star.setOnCheckedChangeListener(this);
+        tick.setOnCheckedChangeListener(this);
+        cancel.setOnCheckedChangeListener(this);
 
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        //int id = buttonView.getId();
 
-
+        Snackbar.make(parent, "value is "+isChecked, Snackbar.LENGTH_SHORT)
+                .setAction("Action", null).show();
 
     }
 
